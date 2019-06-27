703d0c18f611363826394073f3949f73a26dfdf7
Roelof Rietbroek (@pretslet) <r.rietje@gmail.com>
2019-06-27 20:58:33 +0200

    better checks on paths

--- install.sh
+++ install.sh
@@ -2,21 +2,29 @@
 #install dotfiles to directory from where this script was executed (e.g. usually  $HOME)
 
 DOTFILEDIR=$(dirname "$0")
+if [ "." == "${DOTFILEDIR}" ]; then
+	echo refusing to execute install script in source directory
+	exit
+fi
+
+
 echo "linking vim stuff to $DOTFILEDIR"
 
 #vim stuff
 if [ -f .vimrc ]; then
-    mkdir -p vimbackup
+    echo "backup old .vimrc in vimbackup"
+	mkdir -p vimbackup
     mv  .vimrc vimbackup/
 fi
 
-if [ -d .vim ]; then
+if [ -d ./.vim ]; then
+    echo "backup old .vim in vimbackup"
     mkdir -p vimbackup
     mv  .vim vimbackup/
 fi
 
-ln -s $DOTFILEDIR/.vim
-ln -s $DOTFILEDIR/.vimrc
+ln -sf $DOTFILEDIR/.vim
+ln -sf $DOTFILEDIR/.vimrc
 #make sure that the plugins are loaded
-vim -c 'PlugInstall|q|q'
+vim -c 'PlugInstall!|q|q'
 
